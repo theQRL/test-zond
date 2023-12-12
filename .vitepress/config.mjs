9bd8cd41a53d3a0e265dd6c03acbd680043a061d
JP Lomas <jp@theqrl.org>
2023-12-12 22:36:45 +0000

    Dropdowns ✨

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -8,9 +8,28 @@ export default defineConfig({
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: "Installation", link: "/install" },
-      { text: "Testing", link: "/testing" },
-      { text: "Reporting", link: "/reporting" },
+      { text: "Installation",
+        items: [
+          { text: "Windows", link: "/windows" },
+          { text: "Linux", link: "/linux" },
+          { text: "Mac", link: "/mac" },
+        ],
+      },
+      { text: "Testing",
+        items: [
+          { text: "Running", link: "/running" },
+          { text: "Getting synced", link: "/syncing" },
+          { text: "Creating a wallet", link: "/creating-wallet" },
+          { text: "Making a transaction", link: "/transaction" },
+        ],
+      },
+      { text: "Reporting",
+        items: [
+          { text: "Error report", link: "/error"},
+          { text: "Bug report", link: "/bug" },
+          { text: "Feature request", link: "/feature" },
+        ],
+      },
     ],
 
     sidebar: [
